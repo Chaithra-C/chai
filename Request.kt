ba224018ccefd8359c65cb376469c26819ed6e82
Chaithra-C <67100958+Chaithra-C@users.noreply.github.com>
2020-06-18 22:03:28 +0530

    Update Request.kt

--- Request.kt
+++ Request.kt
@@ -1,15 +1,15 @@
-package New.API.Apidemo
+package New.API.Apidemo                                         //Creating the package
 
-import android.util.Log
-import java.net.URL
+import android.util.Log                                         //It is used to handle Error, warning and info logs 
+import java.net.URL                                             //It is used to Identify the http or ftp host
 
-class Request(Private val url: String){
+class Request(Private val url: String){                         //Declaring a Request Class
 
-    fun run() {
-        val url1 = C:/Users/chaithra/Downloads/Kotlin/Anti-heroes
+    fun run() {                                                 //Declaring function
+        val url1 = C:/Users/chaithra/Downloads/Kotlin/Anti-heroes   //Calling external api's
         val url2 = C:/Users/chaithra/Downloads/Kotlin/Mutant
         val url3 = C:/Users/chaithra/Downloads/Kotlin/Avengers
-        Log.d(javaClass.name, forecastJson)
+        Log.d(javaClass.name, forecastJson)                         //Calling java class with name variable.
         Request(url).run()
     }
 }
