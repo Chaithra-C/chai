5ff4da737c81010393104b9f75aaa4e1344e06c1
Chaithra-C <67100958+Chaithra-C@users.noreply.github.com>
2020-06-18 22:18:50 +0530

    Update controller.kt

--- controller.kt
+++ controller.kt
@@ -1,16 +1,16 @@
-package New.API.Apidemo
+package New.API.Apidemo                                                 //Creating the Package
 
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.ReqsponseBody
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController           //It is used to create a restfull controlles
+import org.springframework.web.bind.annotation.RequestMapping           //It is used for mapping web requests onto methods in request-handling classes with flexible method signatures
+import org.springframework.stereotype.Controller                        //It is  is used in which the central type is the Controller class. 
+import org.springframework.web.bind.annotation.ResponseBody            //It is used to  indicates a method return value should be bound to the web response body
+import org.springframework.web.bind.annotation.RequestParam            //It is a Spring annotation used to bind a web request parameter to a method parameter.
 
-@Controller
-class Controller
+@Controller                                                            //It is an annotation for controller
+class Controller                                                       //Creating a class
 {
-    @Autowired
-    var service : Service = Service()
+    @Autowired                                                         //Autowiring feature of spring framework enables you to inject the object dependency implicitly
+    var service : Service = Service()                                  //Creating an object
     @RequestMapping(_value "/")
 
     fun home(@RequestParam name: String, model : Model) : String
